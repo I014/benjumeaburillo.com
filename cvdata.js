e8a0cea4e9a7b068ee0d3a8a4d59b9ba68abff4e
Jorge Benjumea <me@intron014.com>
2025-01-12 00:27:27 +0100

    more cv stuff

--- cvdata.js
+++ cvdata.js
@@ -36,6 +36,14 @@ const cvData = {
                     'allowing for fine-grained filtering by fuel type, brand and prices. It also allows users to see a map view of the stations.',
         link: 'https://gasoprice.com',
         tags: ['PWA', 'Flask', 'Python', 'API']
+      },
+      {
+        title: 'API',
+        description: 'Developed using the [Vapor](https://vapor.codes/) framework an API to replace my old inefficient Flask API, with various endpoints ' +
+               'such as: Retrieving bus times, fetching my favorite stations, grabbing the song I am currently listening to and a few other more ' +
+               '(such as serving the .js used to display this very list)',
+        link: 'https://github.com/Intron014/api.intron014.com',
+        tags: ['Swift', 'API', 'Vapor']
       }
     ]
 };
